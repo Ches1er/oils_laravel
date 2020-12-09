c4139690b1a819b529bed179000726c85a96adbd
Ivan <chester940@gmail.com>
2020-12-09 09:16:53 +0200

    to 500 issue

--- app/Traits/getToObjWithAddProperties.php
+++ app/Traits/getToObjWithAddProperties.php
@@ -70,7 +70,7 @@ trait getToObjWithAddProperties
         $goods = DB::table('to_goods');
         $min = $goods->where('id_group',$id)->whereIn('id', $goods_ids)->min('price');
         echo 'min';
-        print_r($goods->whereIn('id', $goods_ids)->where('price',$min)->get());
+        print_r($goods->whereIn('id', $goods_ids)->get());
         return $goods->whereIn('id', $goods_ids)->where('price',$min)->first();
     }
 }
