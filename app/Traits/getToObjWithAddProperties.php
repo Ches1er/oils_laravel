dec411cb4212798fe62d353f0a20148c70592e10
Ivan <chester940@gmail.com>
2020-12-09 08:56:41 +0200

    to 500 issue

--- app/Traits/getToObjWithAddProperties.php
+++ app/Traits/getToObjWithAddProperties.php
@@ -39,9 +39,9 @@ trait getToObjWithAddProperties
           // print_r($goods_ids);
           $groups = DB::table('to_groups')->select('id')->whereIn('id', $groups_ids)->get();
           foreach ($groups as $group){
-              echo count(DB::table('to_goods')->where('id_group',$group->id)->get()).';';
               if (count(DB::table('to_goods')->where('id_group',$group->id)->get()))
               {
+                  echo $group->id .':'. count(DB::table('to_goods')->where('id_group',$group->id)->get()).';';
                   $max_id = $this->getMax($group->id, $goods_ids)->id;
                   $min_id = $this->getMin($group->id, $goods_ids)->id;
 /*                  echo 'group_id: ' . $group->id . ' ; ';
